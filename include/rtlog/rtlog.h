898056face5a557bde8c69f5b78bbd8213a9f6c3
Chris Apple <14171107+cjappl@users.noreply.github.com>
2023-04-15 11:48:38 -0700

    Basic bones together

--- include/rtlog/rtlog.h
+++ include/rtlog/rtlog.h
@@ -3,19 +3,111 @@
 
 #pragma once
 
+#include <array>
 #include <cstdarg>
 #include <cstdio>
 
+#include <readerwriterqueue.h>
+
 namespace rtlog 
 {
-    class Logger
+
+// TODO Rip this stuff out and templatize it
+constexpr auto MAX_LOG_MESSAGE_LENGTH = 256;
+
+enum class ExampleLogLevel
+{
+    Debug,
+    Info,
+    Warning,
+    Error
+};
+
+const char* to_string(ExampleLogLevel level)
+{
+    switch (level)
     {
-    public:
-        void Log(const char* fstring, ...) { 
-            va_list args;
-            va_start(args, fstring);
-            vprintf(fstring, args);
-            va_end(args);
+    case ExampleLogLevel::Debug:
+        return "Debug";
+    case ExampleLogLevel::Info:
+        return "Info";
+    case ExampleLogLevel::Warning:
+        return "Warning";
+    case ExampleLogLevel::Error:
+        return "Error";
+    default:
+        return "Unknown";
+    }
+}
+
+enum class ExampleLogRegion
+{
+    Engine,
+    Game,
+    Network,
+    Audio
+};
+
+const char* to_string(ExampleLogRegion region)
+{
+    switch (region)
+    {
+    case ExampleLogRegion::Engine:
+        return "Engine";
+    case ExampleLogRegion::Game:
+        return "Game";
+    case ExampleLogRegion::Network:
+        return "Network";
+    case ExampleLogRegion::Audio:
+        return "Audio";
+    default:
+        return "Unknown";
+    }
+}
+
+struct ExampleLogData
+{
+    ExampleLogLevel level;
+    ExampleLogRegion region;
+    std::array<char, MAX_LOG_MESSAGE_LENGTH> message;
+};
+
+class Logger
+{
+public:
+    void Log(ExampleLogLevel level, ExampleLogRegion region, const char* format, ...)
+    {
+        ExampleLogData data;
+        data.level = level;
+        data.region = region;
+
+        va_list args;
+        va_start(args, format);
+        vsnprintf(data.message.data(), data.message.size(), format, args);
+        va_end(args);
+
+        bool result = mQueue.try_enqueue(data);
+        assert(result);
+    }
+
+    void ProcessLog() 
+    {
+        ExampleLogData value;
+        while (mQueue.try_dequeue(value)) 
+        {
+            ExamplePrintMessage(value);
         }
-    };
+    }
+
+    void ExamplePrintMessage(const ExampleLogData& data)
+    {
+        printf("[%s] (%s): %s\n", 
+            rtlog::to_string(data.level), 
+            rtlog::to_string(data.region), 
+            data.message.data());
+    }
+private:
+    moodycamel::ReaderWriterQueue<ExampleLogData> mQueue;
+};
+
 }
--- test/test_rtlog.cpp
+++ test/test_rtlog.cpp
@@ -1,6 +1,8 @@
 #include <doctest/doctest.h>
 #include <rtlog/rtlog.h>
 
+using namespace rtlog;
+
 TEST_CASE("Dummy test")
 {
     CHECK(true);
@@ -9,5 +11,13 @@ TEST_CASE("Dummy test")
 TEST_CASE("Test rtlog")
 {
     rtlog::Logger logger;
-    logger.Log("Hello, world!");
+    logger.Log(ExampleLogLevel::Debug, ExampleLogRegion::Engine, "Hello, world!");
+    logger.Log(ExampleLogLevel::Info, ExampleLogRegion::Game, "Hello, world!");
+    logger.Log(ExampleLogLevel::Warning, ExampleLogRegion::Network, "Hello, world!");
+    logger.Log(ExampleLogLevel::Error, ExampleLogRegion::Audio, "Hello, world!");
+
+    logger.ProcessLog();
+    logger.ProcessLog();
+    logger.ProcessLog();
+    logger.ProcessLog();
 }
